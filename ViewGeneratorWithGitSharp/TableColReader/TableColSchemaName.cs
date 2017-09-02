dd5e5be58b8df1194f35312d08ff79ed4841b9b3
Pieter Engelbrecht <pgengel@gmail.com>
2017-09-02 08:55:57 +0200

    project init

---
+++ ViewGeneratorWithGitSharp/TableColReader/TableColSchemaName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewGeneratorWithGitSharp.TableColReader
+{
+  class TableColSchemaName : TableSchemaName
+  {
+    public List<string> ColumnName { get; set; }
+  }
+}
---
+++ ViewGeneratorWithGitSharp/TableColReader/TableSchemaName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewGeneratorWithGitSharp.TableColReader
+{
+  class TableSchemaName
+  {
+    public string Name { get; set; }
+    public string Schema { get; set; }
+  }
+}
